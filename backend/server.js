936637a69220b6a98adaa042d3df093bcb6f5ee6
Osama <osama22@gmail.com>
2026-06-23 01:03:51 +0300

    solvees

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ import mockupRouter from './routes/mockupRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 4000
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors())
 
 app.use('/api/user',userRouter)
